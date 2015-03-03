bebf9917c25fcafcbe6b5405ac0be13286e935b6
mickaushy <mk516nightscape@gmail.com>
2015-03-04 08:36:19 +0900

    evil disabled

--- init.el
+++ init.el
@@ -21,8 +21,8 @@
 (require 'undo-tree)
 (require 'rainbow-delimiters)
 
-(require 'evil)
-(evil-mode 1)
+;;(require 'evil)
+;;(evil-mode 1)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -33,10 +33,31 @@
 
 (set-face-attribute 'default nil :family "Ricty" :height 120)
 
-(require 'powerline-evil)
-(powerline-evil-vim-color-theme)
+;;(require 'powerline-evil)
+;;(powerline-evil-vim-color-theme)
 
 (require 'emms-snetup)
 (require 'emms-i18n)
 (emms-standard)
 ;todo: install emacs-window-manager, autocomp
+
+(require 'eww)
+;;; [http://rubikitch.com/2014/11/19/eww-nocolor/]背景・文字色を無効化する
+(defvar eww-disable-colorize t)
+(defun shr-colorize-region--disable (orig start end fg &optional bg &rest _)
+  (unless eww-disable-colorize
+    (funcall orig start end fg)))
+(advice-add 'shr-colorize-region :around 'shr-colorize-region--disable)
+(advice-add 'eww-colorize-region :around 'shr-colorize-region--disable)
+(defun eww-disable-color ()
+  "ewwで文字色を反映させない"
+  (interactive)
+  (setq-local eww-disable-colorize t)
+  (eww-reload))
+(defun eww-enable-color ()
+  "ewwで文字色を反映させる"
+  (interactive)
+  (setq-local eww-disable-colorize nil)
+  (eww-reload))
+
+

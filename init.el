5824a5d9acc3035fcd3e0d1eda97cf093a2ab01d
mickaushy <mk516nightscape@gmail.com>
2015-03-18 00:31:39 +0900

    rebuilding init

--- init.el
+++ init.el
@@ -1,29 +1,36 @@
 ;;
 ;; init.el
+;; author: mickaushy
+;; -*- coding: utf-8 -*-
 ;;
 
-;; Language
-(set-language-environment 'Japanese)
-
-;; code
+;; encode
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-buffer-file-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+(setq file-name-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
 
-;;Package Management
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marbmalade" . "http://marmalade-repo.org/packages/"))
-(package-initialize)
+;; behaviour
+(scroll-bar-mode 0)
+(tool-bar-mode 0)
 
-(require 'undo-tree)
-(require 'rainbow-delimiters)
+(setq-default tab-width 4 indent-tabs-mode nil)
 
-;;(require 'evil)
-;;(evil-mode 1)
+;; package
+;; (require 'package)
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.milkboxnet/packages/") t)
+;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (package-initialize)
+(require 'cask "cask.el")
+(cask-initialize)
+(require 'pallet)
 
+;; colour & font
+(set-face-attribute 'default nil :family "Ricty" :height 120)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -31,33 +38,4 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (adwaita))))
 
-(set-face-attribute 'default nil :family "Ricty" :height 120)
-
-;;(require 'powerline-evil)
-;;(powerline-evil-vim-color-theme)
-
-(require 'emms-snetup)
-(require 'emms-i18n)
-(emms-standard)
-;todo: install emacs-window-manager, autocomp
-
-(require 'eww)
-;;; [http://rubikitch.com/2014/11/19/eww-nocolor/]背景・文字色を無効化する
-(defvar eww-disable-colorize t)
-(defun shr-colorize-region--disable (orig start end fg &optional bg &rest _)
-  (unless eww-disable-colorize
-    (funcall orig start end fg)))
-(advice-add 'shr-colorize-region :around 'shr-colorize-region--disable)
-(advice-add 'eww-colorize-region :around 'shr-colorize-region--disable)
-(defun eww-disable-color ()
-  "ewwで文字色を反映させない"
-  (interactive)
-  (setq-local eww-disable-colorize t)
-  (eww-reload))
-(defun eww-enable-color ()
-  "ewwで文字色を反映させる"
-  (interactive)
-  (setq-local eww-disable-colorize nil)
-  (eww-reload))
-
-
+;; packages

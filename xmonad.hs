881e44e1766aa47a3c325391dcf1bed72b01f587
mickaushy <mk516nightscape@gmail.com>
2015-03-30 23:40:09 +0900

    spacing

--- xmonad.hs
+++ xmonad.hs
@@ -5,6 +5,7 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig
 import XMonad.Util.Run                  -- spawnPipe, hPutStrLn
 
@@ -19,7 +20,13 @@ main = do
 
 myModMask = mod4Mask
 
-myLayoutHook = avoidStruts $ layoutHook defaultConfig
+-- myLayoutHook = avoidStruts $ layoutHook defaultConfig
+myLayoutHook = avoidStruts (tiled ||| Mirror tiled ||| Full)
+    where
+        tiled = spacing 5 $ Tall nmaster delta ratio
+        nmaster = 1
+        delta = 3/100
+        ratio = 2/3
 myManageHook = manageDocks <+> manageHook defaultConfig
 
 myLogHook h = dynamicLogWithPP xmobarPP {
